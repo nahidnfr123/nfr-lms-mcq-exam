e91e7d461849129159342390ff73b9ae9eae3709
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2022-08-08 00:53:25 +0600

    Init

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app light>
+    <v-container fluid>
+      <div>
+        <nuxt/>
+      </div>
+    </v-container>
+  </v-app>
+</template>
+
+<style>
+body{
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <nuxt-link to="1">
+      <v-btn>Exam</v-btn>
+    </nuxt-link>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'ContentPage',
+}
+</script>
